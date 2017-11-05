d9a63175606df0fb75ca3747f5360fab456a630d
Jesper Bylund <jesper@blankpage.io>
2017-11-05 16:30:56 +0100

    Installed Gatsby and set up a new static page

---
+++ .cache/async-requires.js
@@ -0,0 +1,26 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/jesperbylund/Development/kodcompany/.cache/dev-404-page.js"),
+  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/jesperbylund/Development/kodcompany/src/pages/404.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/jesperbylund/Development/kodcompany/src/pages/index.js"),
+  "component---src-pages-page-2-js": require("gatsby-module-loader?name=component---src-pages-page-2-js!/Users/jesperbylund/Development/kodcompany/src/pages/page-2.js")
+}
+
+exports.json = {
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/jesperbylund/Development/kodcompany/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/jesperbylund/Development/kodcompany/.cache/json/dev-404-page.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/jesperbylund/Development/kodcompany/.cache/json/layout-index.json"),
+  "404.json": require("gatsby-module-loader?name=path---404!/Users/jesperbylund/Development/kodcompany/.cache/json/404.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/jesperbylund/Development/kodcompany/.cache/json/layout-index.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/Users/jesperbylund/Development/kodcompany/.cache/json/index.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/jesperbylund/Development/kodcompany/.cache/json/layout-index.json"),
+  "page-2.json": require("gatsby-module-loader?name=path---page-2!/Users/jesperbylund/Development/kodcompany/.cache/json/page-2.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/jesperbylund/Development/kodcompany/.cache/json/layout-index.json"),
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/jesperbylund/Development/kodcompany/.cache/json/404-html.json")
+}
+
+exports.layouts = {
+  "component---src-layouts-index-js": require("gatsby-module-loader?name=component---src-layouts-index-js!/Users/jesperbylund/Development/kodcompany/.cache/layouts/index.js")
+}
